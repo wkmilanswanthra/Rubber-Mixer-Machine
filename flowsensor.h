20e3087dabe27a83280ae5cd8509372c611bcdb5
Ishanka Perera <95134409+ishanka020@users.noreply.github.com>
2022-05-21 15:44:27 +0530

    Update flowsensor.h

--- flowsensor.h
+++ flowsensor.h
@@ -18,25 +18,28 @@ void timer_1();
 
 float calibrationFactor = 4.5;
 float flowRate=0;
-volatile unsigned int pulseCount=0;
+volatile unsigned int pulseCount1=0;
+volatile unsigned int pulseCount2=0;
+volatile unsigned int pulseCount3=0;
 unsigned long oldTime=0;
 unsigned int flowMilliLitres=0;
 unsigned long totalMilliLitres=0;
 volatile unsigned long int millisec=0;
 volatile unsigned long int sec=0;
-int v_water=5000;
+int v_water;
+int v_latex;
+int v_acid;
 
 void timer_1(){
 
-	TCCR1A = (0 << WGM11)| (0 << WGM10 );
-	TCCR1B = (0 << WGM12)| (0 << WGM13 );// normal mode
-	TCCR1B |= (1 << CS12) | (1 << CS10);// set pre scaler to 1024 and start the timer
-	TCNT1=0;
+	TCCR1A =(0 << WGM11)| (0 << WGM10 );// CTC
+ 	TIMSK |= (1 << OCIE1A);//Set interrupt on compare match
+ 	TCCR1B |= (1 << CS12) | (1 << CS10);// set pre scaler to 1024 and start the timer
+ 	OCR1A = 0x07;
 }
 
 void pump_w(){
-	PORTB = PORTB | (0<<4);
-	PORTB = PORTB | (0<<3);    /*multiplexer input*/
+	
 	PORTB = PORTB | (1<<5);    /* Turn on the Relay and motor ON*/
 	timer_1();
 	DDRD &= ~(1 << DDD2);     /*Clear the PD2 pin*/
@@ -54,25 +57,27 @@ void pump_w(){
 			{
 				cli();
 				
-				flowRate = ((1000.0 / (millisec - oldTime)) * pulseCount) / calibrationFactor;
+				flowRate = ((1000.0 / (millisec - oldTime)) * pulseCount1) / calibrationFactor;
 				oldTime = millisec;
 				flowMilliLitres = (flowRate / 60) * 1000;
 				totalMilliLitres += flowMilliLitres;
+				
+				millisec=0;
+				sec=0;
+				pulseCount1 = 0;
+
+				sei();
 			}
 		}
 	}
 	PORTB = PORTB & (~(1<<5)); /*Turn off the relay*/
-	millisec=0;
-	sec=0;
-	pulseCount = 0;
 	
-	sei();
 }
 ISR (TIMER1_COMPA_vect){
 	millisec++;
 }
 ISR (INT0_vect){
-	pulseCount++;
+	pulseCount1++;
 }
 
 void pump_l(){
@@ -81,12 +86,12 @@ void pump_l(){
 	DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
 	PORTD = 0xFF;   // turn On the Pull-up
 	DDRD &= ~(1<<PD3);		/* Make INT1 pin as Input */
-	GICR = (1<<INT1);		/* Enable INT0*/
+	GICR = (1<<INT1);		/* Enable INT1*/
 	MCUCR = ((1<<ISC10)|(1<<ISC11));/* Trigger INT1 on Rising Edge triggered */
 
 	sei();// enable interrupts
 	
-	while (totalMilliLitres!=v_water*2)   {
+	while (totalMilliLitres!=v_latex)   {
 		if((millisec-oldTime)>=1000)
 		{
 			sec++;
@@ -94,41 +99,43 @@ void pump_l(){
 			{
 				cli();
 				
-				flowRate = ((1000.0 / (millisec - oldTime)) * pulseCount) / calibrationFactor;
+				flowRate = ((1000.0 / (millisec - oldTime)) * pulseCount2) / calibrationFactor;
 				oldTime = millisec;
 				flowMilliLitres = (flowRate / 60) * 1000;
 				totalMilliLitres += flowMilliLitres;
 
-				
+				millisec=0;
+				sec=0;
+				pulseCount2 = 0;
+
+				sei();
 			}
 		}
 	}
 	PORTB = PORTB & (~(1<<6)); /*Turn off the relay*/
-	millisec=0;
-	sec=0;
-	pulseCount = 0;
 	
-	sei();
 }
 
 
 ISR (INT1_vect){
-	pulseCount++;
+	pulseCount2++;
 }
 
 void pump_a(){
+	PORTB = PORTB | (0<<4);
+	PORTB = PORTB | (0<<3);    /*multiplexer input*/
 	PORTB = PORTB | (1<<7); /* Turn OFF the Relay and motor ON*/
 	timer_1();
 	DDRB &= ~(1 << DDB2);     // Clear the PD2 pin
 	PORTB = 0xFF;   // turn On the Pull-up
 	DDRB &= ~(1<<PB2);		/* Make INT2 pin as Input */
-	GICR = (1<<INT2);		/* Enable INT0*/
+	GICR = (1<<INT2);		/* Enable INT2*/
 	MCUCR = (1<<ISC2);/* Trigger INT2 on Rising Edge triggered */
 
 
 	sei();// enable interrupts
 	
-	while (totalMilliLitres!=v_water/10)   {
+	while (totalMilliLitres!=v_acid)   {
 		if((millisec-oldTime)>=1000)
 		{
 			sec++;
@@ -136,24 +143,25 @@ void pump_a(){
 			{
 				cli();
 				
-				flowRate = ((1000.0 / (millisec - oldTime)) * pulseCount) / calibrationFactor;
+				flowRate = ((1000.0 / (millisec - oldTime)) * pulseCount3) / calibrationFactor;
 				oldTime = millisec;
 				flowMilliLitres = (flowRate / 60) * 1000;
 				totalMilliLitres += flowMilliLitres;
+				
+				millisec=0;
+				sec=0;
+				pulseCount3 = 0;
 
+				sei();
 				
 			}
 		}
 	}
 	PORTB = PORTB & (~(1<<7)); /*Turn off the relay*/
-	millisec=0;
-	sec=0;
-	pulseCount = 0;
 	
-	sei();
 }
 
 
 ISR (INT2_vect){
-	pulseCount++;
+	pulseCount3++;
 }
